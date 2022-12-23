556b9109e50bfca554bd9ad9b4f9f4d36d03f181
Yixuan WANG <wang.yixuan@live.fr>
2022-12-23 10:26:41 -0500

    Add email validation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Main.css";
 import Dashboard from "../images/illustration-dashboard.png";
+import { useState } from "react";
 
 const Main = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleEmail = (e) => {
+    const rgExp =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    e.preventDefault();
+    if (email === "") {
+      setError("Whoops! It looks like you forgot to add your email");
+    } else if (!email.includes("@")) {
+      setError("Please provide a valid email address");
+    } else if (!rgExp.test(email)) {
+      setError("Please provide a valid email address");
+    } else {
+      setError(false);
+      setEmail("");
+    }
+  };
+
   return (
     <div className='main'>
       <div className='input-wrapper'>
@@ -11,13 +31,18 @@ const Main = () => {
           soon!
         </h1>
         <p>Subscribe and get notified</p>
-        <div className='input-block'>
+        <form className='input-block' onSubmit={handleEmail}>
           <div className='input-items'>
-            <input type='email' required />
-            <div className='error'>Please provide a valid email address</div>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type='email'
+              className={error ? "error-border" : "no-error-border"}
+            />
+            <div className={error ? "error" : "no-error"}>{error}</div>
           </div>
           <button>Notify Me</button>
-        </div>
+        </form>
       </div>
       <img src={Dashboard} alt='Dashboard' />
     </div>
